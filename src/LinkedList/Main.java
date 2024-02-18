e56375233c7fe5c6472a8d1074be672571b004dc
JudeJin <jjd1024@foxmail.com>
2024-02-18 17:47:17 -0400

    initial

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -37,34 +37,5 @@ public class Main {
         // Afficher la liste après l'ajout
         System.out.println("Liste après l'ajout de 25 :");
         list.printList();
-
-        CircularQueue queue = new CircularQueue();
-        queue.enqueue(10);
-        queue.enqueue(20);
-        queue.enqueue(30);
-        queue.enqueue(40);
-        queue.enqueue(50);
-        System.out.println("Queue after enqueuing 10, 20, 30, 40, 50:");
-        queue.print();
-        System.out.println("Dequeue: " + queue.dequeue());
-        System.out.println("Queue after dequeuing:");
-        queue.print();
-        System.out.println("Front: " + queue.front());
-        System.out.println("Rear: " + queue.rear());
-        System.out.println("Is 30 in the queue? " + queue.checkInQueue(30));
-        queue.remove(30);
-        System.out.println("Queue after removing 30:");
-        queue.print();
-        queue.reverse();
-        System.out.println("Queue after reversing:");
-        queue.print();
-
-        int[][] grid = {
-                {1, 2, 0},
-                {0, 0, 1}
-        };
-        SurvivalGame game = new SurvivalGame(grid);
-        int iterations = game.play();
-        System.out.println("Nombre d'itérations nécessaires pour infecter toutes les personnes non immunisées : " + iterations);
     }
 }
